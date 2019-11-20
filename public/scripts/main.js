5cbfd831584bad7245a940c2b80e2d419102e72e
Sarah Pugh <sarah.pugh@nathean.com>
2019-11-20 13:33:20 +0000

    Profile

---
+++ public/scripts/main.js
@@ -0,0 +1,152 @@
+'use strict';
+
+// Signs-in Accommodate.
+function signIn() {
+  // Sign into Firebase using popup auth & Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+}
+
+// Signs-out of Accommodate.
+function signOut() {
+  // Sign out of Firebase.
+  firebase.auth().signOut();
+}
+
+// Initiate firebase auth.
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+}
+
+// Returns the signed-in user's display name.
+function getUserName() {
+  return firebase.auth().currentUser.displayName;
+}
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) { // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userNameElement.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    userNameElement.removeAttribute('hidden');
+    userPicElement.removeAttribute('hidden');
+    signOutButtonElement.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    signInButtonElement.setAttribute('hidden', 'true');
+
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    userNameElement.setAttribute('hidden', 'true');
+    userPicElement.setAttribute('hidden', 'true');
+    signOutButtonElement.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    signInButtonElement.removeAttribute('hidden');
+  }
+}
+
+// Adds a size to Google Profile pics URLs.
+function addSizeToGoogleProfilePic(url) {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+}
+
+// Enables or disables the submit button depending on the values of the input
+// fields.
+function toggleButton() {
+  if (messageInputElement.value) {
+    submitButtonElement.removeAttribute('disabled');
+  } else {
+    submitButtonElement.setAttribute('disabled', 'true');
+  }
+}
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+function checkSetup() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
+  }
+}
+
+// Checks that Firebase has been imported.
+checkSetup();
+
+// Shortcuts to DOM Elements.
+var userPicElement = document.getElementById('user-pic');
+var userNameElement = document.getElementById('user-name');
+var signInButtonElement = document.getElementById('sign-in');
+var signOutButtonElement = document.getElementById('sign-out');
+var signInSnackbarElement = document.getElementById('must-signin-snackbar');
+
+// Saves message on form submit.
+// messageFormElement.addEventListener('submit', onMessageFormSubmit);
+signOutButtonElement.addEventListener('click', signOut);
+signInButtonElement.addEventListener('click', signIn);
+
+// initialize Firebase
+initFirebaseAuth();
+
+// Remove the warning about timstamps change. 
+var firestore = firebase.firestore();
+var settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+
+// // Opens up Profile Window when name is clicked
+// function OpenProfileWindow() {
+//   var popup = document.getElementById("profileWindow");
+//   popup.classList.toggle("show");
+// }
+
+// // Open up the Profile Window
+// var modal = document.getElementById("profileWindow");
+// // Open the modal by clicking the username
+// var modalButton = document.getElementById("user-name");
+// // Close button
+// var closeButton = document.getElementsByClassName("closeButton")[0];
+
+// // Listen for open click
+// modalButton.addEventListener('click', openModal);
+// // Listen for close click
+// closeButton.addEventListener('click', closeModal);
+// // Listen for outside click
+// window.addEventListener('click', clickOutside);
+
+// // Function to open modal
+// function openModal() {
+//   modal.style.display = 'block';
+// }
+
+// // Function to close modal
+// function closeModal() {
+//   modal.style.display = 'none';
+// }
+
+// // Function to close modal if outside click
+// function outsideClick(e) {
+//   if(e.target == modal){
+//     modal.style.display = 'none';
+//   }  
+// }
